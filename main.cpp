941360c843e40751c9efa76e89aabf4cc6dda3db
wwoodpacker <gum_dem@ukr.net>
2016-02-18 18:14:50 +0200

    second

--- main.cpp
+++ main.cpp
@@ -16,14 +16,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    /*QSplitter *splitter = new QSplitter;
-    QDirModel *model = new QDirModel;
-    QListView *list = new QListView(splitter);
-         list->setModel(model);
-         list->setRootIndex(model->index(QDir::currentPath()));
-         splitter->setWindowTitle("Two views onto the same directory model");
-             splitter->show();
-*/
+
+
     w.show();
 
     return a.exec();
